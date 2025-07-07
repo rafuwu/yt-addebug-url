2e70b18bd5ec7f1f9afa915ab786c0d38e377063
rafuwu <73766909+rafuwu@users.noreply.github.com>
2025-07-08 00:24:23 +0100

    Inline icon at built time

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import fs from 'node:fs';
 
 export default defineConfig({
-	plugins: [ viteSingleFile({ 
-        removeViteModuleLoader: true,
-    }) ],
+	plugins: [
+        viteSingleFile({ 
+            removeViteModuleLoader: true,
+        }),
+        {
+            name: 'inline-icon', enforce: 'post',
+            transformIndexHtml(html) {
+                const icon_base64 = fs.readFileSync('./assets/icon.png').toString('base64');
+                const link_tag_icon = `<link rel="icon" type="image/png" href="data:image/png;base64,${icon_base64}">`;
+                return html.replace(/<link\srel="icon".*?>/, link_tag_icon);
+            }
+        },
+    ],
     build: {
 		minify: false,
         assetsInlineLimit: 0,
